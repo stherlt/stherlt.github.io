1718e7c1e0f4b1bae7341368af31e2c648f0f851
stherlt <151394883+stherlt@users.noreply.github.com>
2024-11-28 00:16:51 -0700

    Quick commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-});
+  site: 'https://stherlt.github.io'
+})
