d538c6c3407b1b4dc6585f32b2d7e0804ec2fdc2
stherlt <stheriau7@gmail.com>
2024-11-28 12:20:24 -0700

    shiki remove

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config'
-import shiki from 'shiki';
+
 
 export default defineConfig({
   site: 'https://stherlt.github.io',
-  markdown: {
-    shikiConfig: {
-      theme: 'min-light',
-    },
-  },
 })
